c969ba747263d59b9eaac8084a9fc9aae918ac6d
simleshahou <13260020301@163.com>
2022-03-10 17:27:34 +0800

    solidity语言特性

---
+++ 2.SOLIDITY语⾔主要特性/test2.sol
@@ -0,0 +1,30 @@
+/*
+⼀个简单的计数器
+Counter：合约状态变量，保存在链上
+count(): 合约函数
+sum(): 合约函数
+*/
+pragma solidity ^0.8.0;
+contract Counter {
+    uint public counter;
+    uint public sum;
+
+    constructor() {
+        counter = 0;
+        sum = 0;
+    }
+
+    function add(uint256 x) public {
+        counter = counter + x;
+    }
+
+    function set(uint256 x) public {
+        counter = x;
+    }
+
+    function summer(uint256 x) public {
+        sum = sum + x;
+    }
+ 
+}
+
